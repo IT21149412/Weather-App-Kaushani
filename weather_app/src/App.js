34c9e9195ec5fb04ad24579817885d777983dd7e
IT21149412 <it21149412@my.sliit.lk>
2023-08-11 00:13:13 +0530

    Weather commit

--- weather_app/src/App.js
+++ weather_app/src/App.js
@@ -34,16 +34,88 @@ function App() {
     <div className="App">
       <h1>Weather Information</h1>
       <div className="weather-container">
-        {weatherData.map(city => (
-          <div key={city.id} className="weather-tile">
-            <h2>{city.name}</h2>
-            <p>Temperature: {city.main.temp} °C</p>
-            <p>Weather: {city.weather[0].description}</p>
-          </div>
-        ))}
+        {weatherData.map(city => {
+          const weatherStatus = city.weather[0].main.toLowerCase(); // Get the main weather status
+          const weatherIcon = city.weather[0].icon; // Get the weather icon code
+
+          // Define background colors based on main weather status
+          let overlayColor;
+          switch (weatherStatus) {
+            case 'thunderstorm':
+              overlayColor = 'rgba(160, 160, 160, 0.5)'; // Light gray overlay for Thunderstorm
+              break;
+            case 'drizzle':
+              overlayColor = 'rgba(165, 214, 218, 0.5)'; // Light teal overlay for Drizzle
+              break;
+            case 'rain':
+              overlayColor = 'rgba(134, 203, 139, 0.5)'; // Light green overlay for Rain
+              break;
+            case 'snow':
+              overlayColor = 'rgba(224, 224, 224, 0.5)'; // Light gray overlay for Snow
+              break;
+            case 'mist':
+            case 'smoke':
+            case 'haze':
+            case 'dust':
+            case 'fog':
+            case 'sand':
+            case 'ash':
+              overlayColor = 'rgba(209, 218, 219, 0.5)'; // Light gray overlay for various conditions
+              break;
+            case 'squall':
+            case 'tornado':
+              overlayColor = 'rgba(98, 125, 140, 0.5)'; // Light blue gray overlay for Squall and Tornado
+              break;
+            case 'clear':
+              overlayColor = 'rgba(255, 176, 96, 0.5)'; // Light orange overlay for Clear
+              break;
+            case 'clouds':
+              overlayColor = 'rgba(139, 158, 172, 0.5)'; // Light blue gray overlay for Clouds
+              break;
+            default:
+              overlayColor = 'rgba(255, 255, 255, 0.5)'; // Default overlay color
+          }
+
+          const tileStyle = {
+            background: `url('https://img.freepik.com/free-vector/gorgeous-clouds-background-with-blue-sky-design_1017-25501.jpg')`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            position: 'relative', // Set position to make overlay work
+          };
+
+          const overlayStyle = {
+            backgroundColor: overlayColor,
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            borderRadius: '12px',
+          };
+
+
+          return (
+            <div
+              key={city.id}
+              className="weather-tile"
+              style={tileStyle} // Apply inline style
+            >
+              <div style={overlayStyle}></div>
+              <div className="tile-content">
+                <h2>{city.name}</h2>
+                <img
+                  src={`http://openweathermap.org/img/w/${weatherIcon}.png`} // Icon URL
+                  alt="Weather Icon"
+                />
+                <p>Temperature: {city.main.temp} °C</p>
+                <p>Weather: {city.weather[0].description}</p>
+                
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
 }
-
 export default App;
